e302ba01ca3cb94217f8e86a45a7e06bf1119da6
george-waf <georgewafulan@gmail.com>
2017-04-04 15:43:29 +0300

    first commit

---
+++ fuzz_buzz_woof.py
@@ -0,0 +1,18 @@
+def solution(N):
+    for loop in range(1,N+1):
+        if loop%3==0 and loop%5==0 and loop%7==0:
+           print ("FizzBuzzWoof")
+        elif loop%3==0 and loop%5==0:
+           print ("FizzBuzz")
+        elif loop%3==0 and loop%7==0:
+           print ("FizzWoof")
+        elif loop%5==0 and loop%7==0:
+           print ("BuzzWoof")
+        elif loop%7==0:
+           print ("Woof")   
+        elif loop%5==0:
+           print ("Buzz")
+        elif loop%3==0:
+           print ("Fizz")   
+        else:
+           print(loop)
